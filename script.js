09591189e7857270733b5e862ce77b5abb0b1a0a
LanceKSamonte <Lance.samonte2004@gmail.com>
2024-11-03 00:56:41 -0400

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+// free public access token
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFuY2V3YW50c3JpY2UiLCJhIjoiY20zMTJtb21yMHFxYzJrcTFhMHFiODJ3bSJ9.zwoA8m9s-cmwxX48vFxHsw';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: [-79.377319, 43.653963], // starting position [lng, lat]
+    zoom: 11 // starting zoom
+});
